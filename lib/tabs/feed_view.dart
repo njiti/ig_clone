b21790614f8b40006ab7bed0507508f13e7dc4e3
njiti <kamaukan77@gmail.com>
2023-11-27 01:48:35 +0300

    added images to feed

--- lib/tabs/feed_view.dart
+++ lib/tabs/feed_view.dart
@@ -1,10 +1,26 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
 class FeedView extends StatelessWidget {
   const FeedView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('Feed Tab'));
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 4.0),
+      child: MasonryGridView.builder(
+          itemCount: 10,
+          physics: const NeverScrollableScrollPhysics(),
+          gridDelegate: const SliverSimpleGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2
+        ),
+        itemBuilder: (context, index) => Padding(
+          padding: const EdgeInsets.all(2.0),
+          child: ClipRRect(
+              borderRadius: BorderRadius.circular(4),
+              child: Image.asset('lib/images/image' + (index + 1).toString() + '.jpg')),
+        ),
+      ),
+    );
   }
 }
